1747219b1e8c67dc5decde0612d39a9399579476
Maalik McBurrows <mmcburrows1@gmail.com>
2020-04-07 18:26:22 -0400

    added reducer to activity jsx component

--- src/components/activity.jsx
+++ src/components/activity.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { eat } from '../redux/actions';
 
-const Activity = props => (
-    <h1>Activity Comonent</h1>
+const Activity = ({ activity, eat }) => (
+    <>
+        <h1>What's the Cat Doing?</h1>
+        <p>The cat is { activity }</p>
+        <button onClick={() => eat()}>Eating</button>
+    </>
 )
 
- export default Activity;
+//connect connects us to redux, parameters include things we want(mapStateToProps) from connect and things we want to do(mapDispatchToProps) from connect
+
+const mapStateToProps = state => {
+    const { activity } = state;
+    return activity;
+}
+
+export default connect(
+     mapStateToProps, //mapStateToProps
+     { eat } //mapDispatchToProps
+     )(Activity);
 
 
